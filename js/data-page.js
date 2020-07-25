e9f9f500d435ada05f2f94fd10028f07f7e1b041
Romashov Aleksey <amromashov@gmail.com>
2020-07-25 18:30:52 +0300

    Add new tooltips

--- js/data-page.js
+++ js/data-page.js
@@ -5,3 +5,12 @@ theme.coolUpdate();
 window.addEventListener('storage', function (event) {
 	theme.coolUpdate();
 })
+
+$(function(){
+    var includes = $('[data-include]');
+    //document.querySelector("[data-include]").innerText;
+    jQuery.each(includes, function(){
+        var file = 'components/' + $(this).data('include') + '.html';
+        $(this).load(file);
+    });
+});
